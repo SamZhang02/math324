3cf99affd5348e1e9c39310b8c3110f5e4387bda
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-03-31 13:25:10 -0400

    fix odd issue where lm() did not run

--- src/r_tools/linear_regression.r
+++ src/r_tools/linear_regression.r
@@ -1,11 +1,12 @@
 # script that performs linear regression given a data frame
 # and a list of variables to use as predictors
 
-data <- read.csv("data.csv")
+data <- read.csv("data.csv", header = TRUE, sep = ",")
 
 predictors <- c("x1", "x2")
 response <- "y"
+formula <- formula(paste(response, "~", paste(predictors, collapse = " + ")))
 
-model <- lm(response ~ ., data = data[, c(predictors, response)])
+model <- lm(formula, data = subset(data[, c(predictors, response)]))
 
 summary(model)
