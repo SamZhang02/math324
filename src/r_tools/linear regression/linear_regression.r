bffe45b1771edbf646a4ae8e580d1b0cb2ba45a7
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-04-17 19:45:26 -0400

    add more fields in script, fix typo in crib sheet

--- src/r_tools/linear regression/linear_regression.r
+++ src/r_tools/linear regression/linear_regression.r
@@ -1,4 +1,4 @@
-data <- read.csv("data.csv", header = TRUE, sep = ",")
+data <- read.csv("data1.csv", header = TRUE, sep = ",")
 
 x <- c("x")
 y <- "y"
@@ -9,6 +9,7 @@ model <- lm(formula, data = data)
 sum <- summary(model)
 
 n <- nrow(data)
+
 x_mean <- mean(data[, x])
 y_mean <- mean(data[, y])
 
@@ -19,6 +20,9 @@ sxy <- sum((data[, x] - x_mean) * (data[, y] - y_mean))
 sse <- syy - sxy^2 / sxx
 mse <- sse / (n-2)
 
+c00 <- sum((data[, x])^2)/(n*sxx)
+c11 = 1/sxx
+
 #summary
 sum
 paste("Mean x", x_mean)
@@ -28,6 +32,12 @@ paste("Sxy ", sxy)
 paste("Syy ", syy)
 paste("Sum Square Error: ", sse)
 paste("Mean Square Error: ", mse)
+paste("c00: ", c00)
+paste("c11: ", c11)
+paste("S: ", sqrt(sse/(n-2)))
 
-# enable for predictions
+
+# confidence interval
+predict(model, newdata = data.frame(x = c(2)), interval = "confidence", level = .99)
+# prediction interval
 predict(model, newdata = data.frame(x = c(2)), interval = "prediction", level = .99)
